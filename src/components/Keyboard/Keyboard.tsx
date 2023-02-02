c85cbfabafb995f41fe2aa3d19f4d7e01685460b
Sergi Casquero <sergicasquero@gmail.com>
2023-02-02 19:02:50 +0100

    Refactor keys generation in Keyboard component

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,19 +1,12 @@
 import Key from "../Key/Key";
 
 const Keyboard = (): JSX.Element => {
+  const keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "delete"];
   return (
     <ol className="keyboard">
-      <Key content="1" className="key" />
-      <Key content="2" className="key" />
-      <Key content="3" className="key" />
-      <Key content="4" className="key" />
-      <Key content="5" className="key" />
-      <Key content="6" className="key" />
-      <Key content="7" className="key" />
-      <Key content="8" className="key" />
-      <Key content="9" className="key" />
-      <Key content="0" className="key" />
-      <Key content={"delete"} className={"key big"} />
+      {keys.map((key) => (
+        <Key content={key} className={`key${key === "delete" ? " big" : ""}`} />
+      ))}
     </ol>
   );
 };
